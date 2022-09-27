f7914613363eed0ca1a905081e609ecd973af683
NhatVD1311 <89086078+NhatVD1311@users.noreply.github.com>
2022-09-27 16:09:28 +0700

    Create rsa.py

---
+++ crypto/RSA noob/rsa.py
@@ -0,0 +1,17 @@
+import gmpy2
+from gmpy2 import mpz
+
+e=mpz(1)
+c=mpz(9327565722767258308650643213344542404592011161659991421)
+n=mpz(245841236512478852752909734912575581815967630033049838269083)
+
+#use factordb
+q=mpz(416064700201658306196320137931)
+p=mpz(590872612825179551336102196593)
+
+phi=gmpy2.mul(p-1,q-1)
+d=gmpy2.invert(e,phi)
+f=gmpy2.powmod(c,d,n)
+g=bytes.fromhex(hex(f)[2:])
+
+print("[+] Flag is : ",g)
